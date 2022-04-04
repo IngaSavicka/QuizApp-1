d8c6cec5755a553f5b0058299d02f095202b542f
IngaSavicka <83701576+IngaSavicka@users.noreply.github.com>
2022-04-04 10:04:31 +0300

    jautajumi
    Jautājumi

--- server.py
+++ server.py
@@ -2,27 +2,42 @@ from flask import Flask
 from flask import request
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(_name_)
+
+def loadAllPictures():
+  filepath = 'picDB.txt'
+  pictures=[]
+  with open(filepath) as fp:
+    line = fp.readline()
+    cnt = 1
+    while line:
+      if line!='':
+        fullPicInfo = line.strip().split(';')
+        pictures.append(fullPicInfo[1])
+      line = fp.readline()
+      cnt += 1
+  fp.close()
+  return pictures
 
 #Pirmā lapa, kas tiks ielādēta
 @app.route('/',methods = ['POST', 'GET'])
 def root():
     return render_template("index.html")
-    
-#Pārbaudes lapa, lai saprastu, ka kods vispār strādā
-@app.route('/health')
-def health():
-  return "OK"
-
-@app.route('/test')
-def test():
-  parametri = ["Augums","Svars","Vecums"]
-  return render_template("test.html",param=parametri)
 
 @app.route('/about')
 def about():
   return render_template("about.html")
 
+@app.route('/test',methods = ['POST', 'GET'])
+def test():
+    parametri = ["IQ","Augums","Kāja"]
+    images = loadAllPictures()
+    return render_template("test.html",parametri=parametri,images=images)
 
-if __name__ == '__main__':
+#Pārbaudes lapa, lai saprastu, ka kods vispār strādā
+@app.route('/health')
+def health():
+  return "Viss kārtībā!"
+
+if _name_ == '_main_':
   app.run(debug="true")
